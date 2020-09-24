339085ca6827b7465af0f20ff20516072688ab82
KhaledAyman2222 <khaledayman2222@gmail.com>
2020-09-24 21:25:56 +0200

    edit

--- app/src/main/java/QuestionHandling/DataRetriever.java
+++ app/src/main/java/QuestionHandling/DataRetriever.java
@@ -23,7 +23,7 @@ public class DataRetriever {
         Cursor cursor = database.rawQuery("SELECT * FROM data", null);
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            if((mode.equals("FtoCa") || mode.equals("CatoF")) && cursor.getString(1).equals(""))
+            if((mode.equals("FtoCa") || mode.equals("CatoF")) && cursor.getString(2).equals(""))
                 {cursor.moveToNext(); continue;}
             data.add(new Country(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3)));
             cursor.moveToNext();
